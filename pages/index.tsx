7fff76ce91de2a5d72b11ea2abd1be979d7fd6c0
Gideon Idoko <iamgideonidoko@gmail.com>
2022-08-27 07:39:59 +0100

    Add test tools

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600 text-center">OgaRepair App</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600 text-center">OgaRepair App</h1>
+    </div>
+  );
 };
 
 export default Home;
