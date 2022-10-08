a8444cb8480eecdda36cab9740c28125653b36be
Vicolas11 <incrediblechamp1@gmail.com>
2022-10-08 11:41:11 +0100

    feat: created client signup page

--- pages/signup/client.tsx
+++ pages/signup/client.tsx
@@ -5,17 +5,24 @@ import Input from '../../components/reusables/Input';
 import { NextPage } from 'next';
 import Link from 'next/link';
 
-const Admin: NextPage = () => {
+const Client: NextPage = () => {
   return (
     <OnboardLayout>
-      <div className="flex flex-col items-center mx-auto w-full translate-y-1/4 space-y-4">
-        <h1 className="text-center font-semibold text-2xl lg:text-3xl">Log in to OgaRepair</h1>
+      <div className="flex flex-col items-center mx-auto w-full mt-7 lg:mt-8 xl:mt-10 translate-y-6/12 space-y-4">
+        <h1 className="text-center font-semibold text-2xl lg:text-3xl">Sign up to connect with clients</h1>
+        <p className="text-center text-sm text-gray-700">
+          Looking to hire clients?{' '}
+          <Link href="/">
+            <a className="text-blue-500 hover:text-blue-700 hover:underline">Sign up as a client</a>
+          </Link>
+        </p>
         <div className="flex flex-col justify-center items-center w-full xl:w-7/12 lg:flex-row lg:space-x-5">
           <OnboardAuthBtn icon="google" />
           <OnboardAuthBtn icon="facebook" />
         </div>
         <h1 className="text-gray-400">-OR-</h1>
         <form className="flex flex-col space-y-5 w-full lg:w-6/12">
+          <Input type="text" name="full_name" placeholder="Full Name" />
           <Input type="email" name="email" placeholder="Email" />
           <Input type="password" name="password" placeholder="Password" />
           <Button type="submit" title="Create Account" />
@@ -31,4 +38,4 @@ const Admin: NextPage = () => {
   );
 };
 
-export default Admin;
+export default Client;
